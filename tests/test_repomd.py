616e48e6db8dad3e0610d2ab7ae4e356d9faa282
Carl George <carl@george.computer>
2017-12-11 11:08:35 -0600

    add test fixture repodata

--- tests/test_repomd.py
+++ tests/test_repomd.py
@@ -1,4 +1,16 @@
+import pytest
+
 import repomd
+
+
+@pytest.fixture
+def repodata():
+    path = 'tests/fixtures/repodata'
+    with open(f'{path}/repomd.xml', 'rb') as f:
+        raw_index = f.read()
+    with open(f'{path}/primary.xml.gz', 'rb') as f:
+        raw_primary = f.read()
+    return (raw_index, raw_primary)
 
 
 def test_repo_init():
