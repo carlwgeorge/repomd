1426cc709d487a9597917422b97bfbb536afc78d
Carl George <carl@george.computer>
2018-04-27 22:36:27 -0500

    switch from repodata fixtures to repo fixtures
    Having the repodata be a fixture is repetitive, because I still need to
    instantiate a Repo object every test.

--- tests/test_repomd.py
+++ tests/test_repomd.py
@@ -7,26 +7,29 @@ import repomd
 
 
 @pytest.fixture
-def repodata():
+@mock.patch('repomd.urlopen')
+def repo(mock_urlopen):
     path = 'tests/fixtures/repodata'
     with open(f'{path}/repomd.xml', 'rb') as f:
         raw_index = f.read()
     with open(f'{path}/primary.xml.gz', 'rb') as f:
         raw_primary = f.read()
-    return (raw_index, raw_primary)
+    mock_urlopen.return_value.__enter__.return_value.read.side_effect = (raw_index, raw_primary)
+    return repomd.Repo('https://example.com')
 
 
-@mock.patch('repomd.urlopen')
-def test_repo_init(mock_urlopen, repodata):
-    mock_urlopen.return_value.__enter__.return_value.read.side_effect = repodata
-    repo = repomd.Repo('https://example.com')
+@pytest.fixture
+def lazy_repo():
+    return repomd.Repo('https://example.com', lazy=True)
+
+
+def test_repo_init(repo):
     assert repo.baseurl == 'https://example.com'
     assert repr(repo) == '<Repo: "https://example.com">'
     assert str(repo) == 'https://example.com'
     assert isinstance(repo._metadata, lxml.etree._Element)
 
 
-def test_repo_init_lazy():
-    repo = repomd.Repo('https://example.com', lazy=True)
-    assert repo.baseurl == 'https://example.com'
-    assert repo._metadata is None
+def test_repo_init_lazy(lazy_repo):
+    assert lazy_repo.baseurl == 'https://example.com'
+    assert lazy_repo._metadata is None
