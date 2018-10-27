f6dcef3dae56e8bd1f301d98ce2d159e63619a50
Carl George <carl@george.computer>
2018-10-27 05:37:43 -0500

    Python packaging fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ with open('README.md') as f:
 setup(
     name='repomd',
     version='0.1.0',
-    description='library for reading dnf/yum repositories',
+    author='Carl George',
+    author_email='carl@george.computer',
+    description='Library for reading dnf/yum repositories',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/carlwgeorge/repomd',
@@ -17,5 +19,11 @@ setup(
     py_modules=['repomd'],
     python_requires='>=3.6',
     setup_requires=['setuptools>=38.6.0'],
+    tests_require=['pytest'],
     install_requires=['lxml'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
 )
