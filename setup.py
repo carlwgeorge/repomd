abff255b30b22417f3922dbfd785f322ff897363
Carl George <carl@george.computer>
2019-03-20 23:28:28 -0500

    Change Python 3 classifier to Python 3 Only

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
     ],
 )
