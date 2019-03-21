316a5709fe82170fb3ea7129efa87291ae9a61b5
Carl George <carl@george.computer>
2019-03-20 23:51:57 -0500

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with pathlib.Path('README.md').open() as f:
 
 setuptools.setup(
     name='repomd',
-    version='0.1.0',
+    version='0.2.0',
     author='Carl George',
     author_email='carl@george.computer',
     description='Library for reading dnf/yum repositories',
